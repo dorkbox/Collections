ac68821bfcdecc08004e8c942682ca4e55a0b733
Robinson <robinson@dorkbox.com>
2023-08-05 11:39:27 -0600

    updated build deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ gradle.startParameter.showStacktrace = ShowStacktrace.ALWAYS   // always show th
 
 plugins {
     id("com.dorkbox.GradleUtils") version "3.17"
-    id("com.dorkbox.Licensing") version "2.22"
+    id("com.dorkbox.Licensing") version "2.25"
     id("com.dorkbox.VersionUpdate") version "2.8"
     id("com.dorkbox.GradlePublish") version "1.18"
 
